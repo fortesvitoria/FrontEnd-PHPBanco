8becb635a1f23b52a2c7a9fc08f2680ba60b6edf
Perkles <fortesvitoria@gmail.com>
2023-11-08 21:31:10 -0300

    adiciona inserir php

---
+++ inserir.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inserir no BD</title>
+</head>
+
+<body>
+
+    <?php
+
+    //importa dos dados do banco com método include()
+    include("conexao.php");
+
+    //cria variáveis com valores digitados na interface (index.html)
+    $nome  = $_POST["nome"];
+    $cpf = $_POST["cpf"];
+    $email = $_POST["email"];
+    $endereco = $_POST["endereco"];
+
+    //query do sql
+    $sql = mysqli_query($banco, "insert into cadastro values(null, '$nome', '$cpf', '$email', '$endereco');");
+
+    if($sql) {
+        echo "Cliente cadastrado :)";
+    } else {
+        echo "Não foi possível cadastrar o cliente :(".mysqli_error($banco);
+    }
+
+    mysqli_close($banco);
+
+
+    ?>
+
+</body>
+
+</html>
